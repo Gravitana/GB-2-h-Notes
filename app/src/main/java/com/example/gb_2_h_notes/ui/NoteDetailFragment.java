dfb41d1498951e2aeeebed8bd639211c06d65316
Vitaliy <v.a.grahov@ya.ru>
2021-05-16 01:01:59 +0500

    Фрагмент с детальной инфой для двух ориентаций

---
+++ app/src/main/java/com/example/gb_2_h_notes/ui/NoteDetailFragment.java
@@ -0,0 +1,74 @@
+package com.example.gb_2_h_notes.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.gb_2_h_notes.R;
+import com.example.gb_2_h_notes.domain.Note;
+
+public class NoteDetailFragment extends Fragment {
+
+    private static final String CURRENT_NOTE = "Current note";
+
+    private TextView id, title, body, date;
+    private ImageView image;
+
+    public NoteDetailFragment() {
+        // Required empty public constructor
+    }
+
+    public static NoteDetailFragment newInstance(Note note) {
+        NoteDetailFragment fragment = new NoteDetailFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(CURRENT_NOTE, note);
+
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        id = view.findViewById(R.id.note_detail_id);
+        title = view.findViewById(R.id.note_detail_title);
+        body = view.findViewById(R.id.note_detail_body);
+        date = view.findViewById(R.id.note_detail_date);
+        image = view.findViewById(R.id.note_detail_image);
+
+        Note note = null;
+        if (getArguments() != null) {
+            note = getArguments().getParcelable(CURRENT_NOTE);
+        }
+
+        if (note != null) {
+            id.setText(note.getStringId());
+            title.setText(note.getTitle());
+            body.setText(note.getBody());
+            date.setText(note.getDate());
+
+            Glide.with(image)
+                    .load(note.getImageUrl())
+                    .centerCrop()
+                    .into(image);
+        }
+    }
+}
--- app/src/main/java/com/example/gb_2_h_notes/ui/list/NoteListFragment.java
+++ app/src/main/java/com/example/gb_2_h_notes/ui/list/NoteListFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,10 +19,14 @@ import android.widget.Toast;
 import com.example.gb_2_h_notes.R;
 import com.example.gb_2_h_notes.domain.MockNotesRepository;
 import com.example.gb_2_h_notes.domain.Note;
+import com.example.gb_2_h_notes.ui.MainActivity;
+import com.example.gb_2_h_notes.ui.NoteDetailFragment;
 
 import java.util.List;
 
 public class NoteListFragment extends Fragment {
+
+    private boolean isLandscape;
 
     public NoteListFragment() {
         // Required empty public constructor
@@ -36,6 +43,8 @@ public class NoteListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        isLandscape = getResources().getBoolean(R.bool.isLandscape);
+
         List<Note> notes = new MockNotesRepository().getNotes();
 
         RecyclerView notesList = view.findViewById(R.id.notes_list);
@@ -51,14 +60,28 @@ public class NoteListFragment extends Fragment {
         adapter.setNotesListItemClickListener(new NotesAdapter.OnNotesListItemClickListener() {
             @Override
             public void onNotesListItemClick(View view, int position) {
-                openNoteDetail(notes.get(position), position);
+                openNoteDetail(view, position, notes.get(position));
             }
         });
 
         adapter.notifyDataSetChanged(); // перерисовка списка
     }
 
-    private void openNoteDetail(Note note, int position) {
-        Toast.makeText(getContext(), note.getTitle() + " (on " + position + " position)", Toast.LENGTH_SHORT).show();
+    private void openNoteDetail(View view, int position, Note note) {
+//        Toast.makeText(getContext(), note.getTitle() + " (on " + position + " position)", Toast.LENGTH_SHORT).show();
+
+        AppCompatActivity activity = (AppCompatActivity) view.getContext();
+
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        if (isLandscape) {
+            transaction.replace(R.id.fragment_ext, NoteDetailFragment.newInstance(note));
+        } else {
+            transaction.replace(R.id.fragment_container, NoteDetailFragment.newInstance(note));
+        }
+
+        transaction.addToBackStack(null).commit();
     }
 }
